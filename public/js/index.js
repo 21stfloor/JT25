8de9363b5b34529bf586a3de56ab517cb5936b77
amiel-danao <21stfloordev@gmail.com>
2023-12-31 21:28:00 +0800

    done quotation list table

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, addDoc, collection, serverTimestamp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCSW7UMHtJDdRJga5oSEHgppvUsLRtagx0",
     authDomain: "jt25-fae27.firebaseapp.com",
@@ -7,14 +11,29 @@ const firebaseConfig = {
     appId: "1:590615766263:web:95dc99f6487ba0693a1ff2",
     measurementId: "G-QB2Z7JDN2V"
   };
+
+  // Your web app's Firebase configuration
+        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+        // const firebaseConfig = {
+        //   apiKey: "AIzaSyCSW7UMHtJDdRJga5oSEHgppvUsLRtagx0",
+        //   authDomain: "jt25-fae27.firebaseapp.com",
+        //   projectId: "jt25-fae27",
+        //   storageBucket: "jt25-fae27.appspot.com",
+        //   messagingSenderId: "590615766263",
+        //   appId: "1:590615766263:web:95dc99f6487ba0693a1ff2",
+        //   measurementId: "G-QB2Z7JDN2V"
+        // };
+      
+        // Initialize Firebase
+        export const app = initializeApp(firebaseConfig);
   
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestore();
-const auth = firebaseApp.auth();
+// export  const firebaseApp = firebase.initializeApp(firebaseConfig);
+// const db = firebaseApp.firestore();
+export const auth = getAuth(app);
 
 
 var currentUser = null;
-firebase.auth().onAuthStateChanged(
+auth.onAuthStateChanged(
     (User)=> {
         currentUser = User;
         if (currentUser) {
